3e0d095621f2f4be969c1c80280be902b88e26d8
Yatika Jena <j.yatika@iitg.ac.in>
2024-07-15 17:21:13 +0530

    added .gitignore

--- qachat.py
+++ qachat.py
@@ -4,7 +4,15 @@ import streamlit as st
 import os
 import google.generativeai as genai
 
-genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+api_key = os.getenv("GOOGLE_API_KEY")
+if not api_key:
+    api_key = st.secrets["GOOGLE_API_KEY"]
+
+if not api_key:
+    raise ValueError("No API key found. Set the GOOGLE_API_KEY environment variable.")
+# Use the API key in your application
+genai.configure(api_key=api_key)
 
 # Function to load Gemini Pro model and get responses
 model = genai.GenerativeModel("gemini-pro") 
