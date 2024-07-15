0693a9c7fe77a1ff447ad818e7691fcf761fdaa9
Yatika Jena <j.yatika@iitg.ac.in>
2024-07-15 16:54:06 +0530

    Update qachat.py

--- qachat.py
+++ qachat.py
@@ -26,7 +26,7 @@ def extract_text_from_response(response):
 
 # Initialize our streamlit app
 st.set_page_config(page_title="Q&A Demo")
-st.header("Gemini LLM Application")
+st.header("Customer Service Chatbot")
 
 # Initialize session state for chat history if it doesn't exist
 if 'chat_history' not in st.session_state:
